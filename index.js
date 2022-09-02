951f0ec2e09729bd8d05874eadadc1b4378754a3
pavan <vermapavan07@gmail.com>
2022-09-02 19:10:50 +0530

    Count People

---
+++ index.js
@@ -0,0 +1,21 @@
+// document.getElementById("count-el").innerText = 5;
+
+// camelCase variable Style
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+// console.log(countEl)
+
+
+let count = 0;
+
+function increment(){
+    
+    count += 1;
+    countEl.innerText = count;
+}
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr;
+    count = 0;
+    countEl.textContent = count;
+}
